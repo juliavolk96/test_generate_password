b4f976542f0d73366542e11ad9b406e353ef0084
Julia Volkova <juliavolk96@gmail.com>
2024-07-29 15:50:15 +0400

    Initial commit

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,21 @@
+class PasswordsController < ApplicationController
+  def new
+    @password = params[:password]
+    @case_option = params[:case] || :lowercase
+    @add_non_letter = params[:add_non_letter] == "1"
+    @password_length = params[:password_length] || 12
+    @non_letter_count = params[:non_letter_count] || 0
+  end
+
+  def create
+    options = {
+      case: params[:case].to_sym,
+      add_non_letter: params[:add_non_letter] == "1",
+      password_length: params[:password_length].to_i,
+      non_letter_count: params[:non_letter_count].to_i
+    }
+    generator = PronouncablePasswordGenerator.new(options)
+    @password = generator.generate
+    render :new
+  end
+end
